24d6f81d9a8ae01485555e579e7f8c1f729562f5
Gabgosq25 <gabgosq25@gmail.com>
2026-06-15 02:52:22 -0300

    funcao split

---
+++ Funcvet.java
@@ -0,0 +1,12 @@
+public class Funcvet {
+    public static void main(String[] args) {
+        String s = "potato apple leon orange";
+        String[] vect = s.split(" ");
+
+        System.out.println(vect[0]);
+        System.out.println(vect[1]);
+        System.out.println(vect[2]);
+        System.out.println(vect[3]);
+    }
+    
+}
